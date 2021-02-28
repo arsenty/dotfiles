e312ee892debc22b41069616946b2de9da85e8f6
arsenty <1301x1@gmail.com>
2021-02-28 14:59:17 +0300

    fix sync

--- dot_shortcuts/org.sync.sh
+++ dot_shortcuts/org.sync.sh
@@ -1,5 +1,5 @@
-#eval `/usr/local/bin/keychain --eval id_github`;
-eval `ssh-agent -s`; ssh-add $HOME/.ssh/id_github;
+eval `/usr/local/bin/keychain --eval id_github`;
+#eval `ssh-agent -s`; ssh-add $HOME/.ssh/id_github;
 cd $HOME/org/;
 if [[ -z `git status --porcelain` ]]; then
     # No changes
