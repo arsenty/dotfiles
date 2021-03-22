40204d2c3eb89fc8c5fef7d00acc9ea059216375
arsenty <1301x1@gmail.com>
2021-03-22 12:00:30 +0300

    org script fix

--- dot_shortcuts/org.reschedule.sh
+++ dot_shortcuts/org.reschedule.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-perl -i -pe "s/SCHEDULED: <`date -d "yesterday" '+%Y-%m-%d %a'`/SCHEDULED <`date '+%Y-%m-%d %a'`/g" ~/org/pages/*.org
+perl -i -pe "s/SCHEDULED: <`date -d "yesterday" '+%Y-%m-%d %a'`/SCHEDULED: <`date '+%Y-%m-%d %a'`/g" ~/org/pages/*.org
