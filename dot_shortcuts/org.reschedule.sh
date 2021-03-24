90bdb131d879784dde4f9a47c485c4caeed8488e
arsenty <1301x1@gmail.com>
2021-03-24 15:38:39 +0300

    upd org script for BSD/Linux date cli

--- dot_shortcuts/org.reschedule.sh
+++ dot_shortcuts/org.reschedule.sh
@@ -1,2 +1,8 @@
 #!bin/bash
-perl -i -pe "s/SCHEDULED: <`date -d "yesterday" '+%Y-%m-%d %a'`/SCHEDULED: <`date '+%Y-%m-%d %a'`/g" ~/org/pages/*.org
+if [ "$(uname)" = "Darwin" ];
+then
+    yesterday='-v -1d'
+else
+    yesterday='-d yesterday'
+fi
+perl -i -pe "s/SCHEDULED: <`date $yesterday '+%Y-%m-%d %a'`/SCHEDULED: <`date '+%Y-%m-%d %a'`/g" ~/org/pages/*.org
