e5fee04e05861b9bdb86da464b2429dd06676b8d
arsenty <1301x1@gmail.com>
2021-06-19 11:23:49 +0300

    upd cron scripts

--- dot_shortcuts/org.reschedule.sh
+++ dot_shortcuts/org.reschedule.sh
@@ -1,8 +1,9 @@
-#!bin/bash
+#!bin/sh
+#0 0 * * * sh $HOME/.shortcuts/org.reschedule.sh
 if [ "$(uname)" = "Darwin" ];
 then
     yesterday='-v -1d'
 else
     yesterday='-d yesterday'
 fi
-perl -i -pe "s/SCHEDULED: <`date $yesterday '+%Y-%m-%d %a'`/SCHEDULED: <`date '+%Y-%m-%d %a'`/g" ~/org/habit*
+perl -i -pe "s/SCHEDULED: <`date $yesterday '+%Y-%m-%d %a'`/SCHEDULED: <`date '+%Y-%m-%d %a'`/g" $HOME/org/habit*
--- dot_shortcuts/org.sync.sh
+++ dot_shortcuts/org.sync.sh
@@ -1,6 +1,8 @@
+#!bin/sh
+# */1 * * * * sh $HOME/.shortcuts/org.sync.sh 2 > $HOME/.shortcuts/org.sync.log
 eval `/usr/local/bin/keychain --eval id_github`;
 #eval `ssh-agent -s`; ssh-add $HOME/.ssh/id_github;
-cd $HOME/Library/Mobile\ Documents/iCloud~com~appsonthemove~beorg/Documents/org/;
+cd $HOME/org/;
 if [[ -z `git status --porcelain` ]]; then
     # No changes
     git pull
