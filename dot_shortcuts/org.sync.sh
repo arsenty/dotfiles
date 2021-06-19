7d2d5457c3383d7eefc654cc9bc6966be25c6013
arsenty <1301x1@gmail.com>
2021-06-19 11:45:12 +0300

    upd cron scripts

--- dot_shortcuts/org.sync.sh
+++ dot_shortcuts/org.sync.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 # */1 * * * * sh $HOME/.shortcuts/org.sync.sh 2 > $HOME/.shortcuts/org.sync.log
-eval `/usr/local/bin/keychain --eval id_github`;
+eval `keychain --eval id_github`;
 #eval `ssh-agent -s`; ssh-add $HOME/.ssh/id_github;
 cd $HOME/org/;
 if [[ -z `git status --porcelain` ]]; then
