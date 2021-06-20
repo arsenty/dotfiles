5a7390eb23e392acbe1cd33bb79a88959b74d839
arsenty <1301x1@gmail.com>
2021-06-20 14:39:27 +0300

    org sync fix

--- dot_shortcuts/org.sync.sh
+++ dot_shortcuts/org.sync.sh
@@ -1,6 +1,7 @@
 #!bin/sh
-# */1 * * * * sh $HOME/.shortcuts/org.sync.sh 2 > $HOME/.shortcuts/org.sync.log
-eval `keychain --eval id_github`;
+# */1 * * * * sh $HOME/.shortcuts/org.sync.sh >2 $HOME/.shortcuts/org.sync.log
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+eval `keychain --eval id_rsa`;
 #eval `ssh-agent -s`; ssh-add $HOME/.ssh/id_github;
 cd $HOME/org/;
 if [[ -z `git status --porcelain` ]]; then
