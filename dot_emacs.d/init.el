2508126296c77df2b0c65ede2417d0b8f4a31ddf
arsenty <1301x1@gmail.com>
2021-06-23 12:06:08 +0300

    emacs cleanup

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -1,11 +1,10 @@
 (setq default-input-method "russian-computer")
 (setq split-width-threshold 0)
 
-(add-to-list 'package-archives
-    '("melpa-stable" . "https://melpa.org/packages/") t)
-
-(load "f")
-(load "~/.emacs.d/init-org")
+;(add-to-list 'package-archives
+    ;'("melpa-stable" . "https://melpa.org/packages/") t)
+;(load "f")
+;(load "~/.emacs.d/init-org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -13,9 +12,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-modules
-   '(ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe ol-rmail ol-w3m))
- '(package-selected-packages '(org helm-org s dash ## f)))
-
+   '(ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe org-mouse ol-rmail ol-w3m org-checklist org-mac-iCal org-mac-link org-notify org-panel))
+ '(package-selected-packages '(org helm-org s f calfw-org calfw dashboard org-super-agenda helm ##))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -23,4 +22,3 @@
  ;; If there is more than one, they won't work right.
  )
 (provide 'init)
-
